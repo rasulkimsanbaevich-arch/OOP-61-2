e1f6bfb6350ba20a296ebdaa320a9504f623ea44
rasul-61-2 <rasulkimsanbaevich@gmail.com>
2025-11-29 17:05:34 +0600

    lesson1

--- lessons/lesson1.py
+++ lessons/lesson1.py
@@ -1,8 +1,22 @@
 class Hero:
-    #конструктор класса
+    # конструктор класса
     def __init__(self, name, lvl, hp):
+        # атрибуты класса
         self.name = name
         self.lvl = lvl
         self.hp = hp
 
+
+    def base_action(self):
+        return f"base action {self.name}"
+
+# экземпляр класса
+kirito = Hero("Kirito", 100, 1000)
+asuna = Hero("Asuna", 100, 100)
+
+just_text = str("just text")
+print(kirito.name)
+print(asuna.hp)
+print(kirito.base_action())
+1:30
 
